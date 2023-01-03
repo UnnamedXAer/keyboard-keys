4d3662c74e54823723605d414e0f1cea1fc19749
UnnamedXAer <unnamedxaer.git@gmail.com>
2023-01-03 21:47:25 +0100

    remove console log

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,7 +2,7 @@ import { loadContent } from '../helpers/text';
 import type { PageLoad } from './$types';
 
 export const load = ((args) => {
-	console.log('LOAD', args);
+	// console.log('LOAD', args);
   return {
     content: loadContent(args.fetch),
   };
